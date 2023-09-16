7420012cb0c0415821a21c6fd65838a1a833812d
mr_sd_jack_003 <barson0habra@gmail.com>
2023-09-16 23:41:49 +0530

    change command

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        console.log("I am going to block:", details.url)
+        console.log("Block this link:", details.url)
         return {cancel: true}
     },
     {urls: blocked_sites_v2},
